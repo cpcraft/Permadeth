0d97f9d40148cb9d65ca4b3cd2a22761516241ee
Aivar <you@example.com>
2025-08-20 20:12:23 +0300

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { readFileSync } from 'fs';
 import pg from 'pg';
 
-const { Pool } = pg;
+const { Pool } = pg; 
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 export async function initSchema() {
